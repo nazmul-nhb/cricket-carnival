89dd9cca72f904797cd550e77e2a1285b31b36f0
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-28 14:10:04 +0600

    created generic functions to get, add and remove item(s) from local storage

---
+++ src/utilities/localStorage.ts
@@ -0,0 +1,32 @@
+export const getItemFromStorage = (key: string): string[] => {
+  const items = localStorage.getItem(key);
+  return JSON.parse(items || '[]') as string[];
+};
+
+export const setItemToStorage = (
+  key: string,
+  item: string,
+): { success: boolean } => {
+  const storedItems = getItemFromStorage(key);
+
+  if (storedItems.includes(item)) {
+    return { success: false };
+  }
+
+  const updatedItems = [...storedItems, item];
+
+  localStorage.setItem(key, JSON.stringify(updatedItems));
+  return { success: true };
+};
+
+export const removeItemFromStorage = (
+  key: string,
+  item: string,
+): { success: boolean } => {
+  const storedItems = getItemFromStorage(key);
+
+  const updatedItems = storedItems.filter(storedItem => storedItem !== item);
+
+  localStorage.setItem(key, JSON.stringify(updatedItems));
+  return { success: true };
+};
